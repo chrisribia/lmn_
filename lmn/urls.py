a3076a3698e0057dd53539619cf7131c78f0fce7
chris ribia <chrisribia@gmail.com>
2023-07-14 11:31:20 +0300

    urls

--- lmn/urls.py
+++ lmn/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('panels/', include('payments.urls')),
+    path('', include('payments.urls')),
 ]
