e92e71d1190126941a1c863d3d8e3dcf7404cc05
chris ribia <chrisribia@gmail.com>
2023-07-14 12:18:14 +0300

    confirm url

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import  home,getAccessToken,lipa_na_mpesa_online
+from .views import  home,getAccessToken,lipa_na_mpesa_online,register_urls,confirmation,validation,call_back
 
 
 urlpatterns = [
     path('', home, name='home'),  
     path('access/token', getAccessToken, name='get_mpesa_access_token'),
     path('online/lipa', lipa_na_mpesa_online, name='lipa_na_mpesa'),    
+
+    # register, confirmation, validation and callback urls
+    path('c2b/register', register_urls, name="register_mpesa_validation"),
+    path('c2b/confirmation', confirmation, name="confirmation"),
+    path('c2b/validation', validation, name="validation"),
+    path('c2b/callback', call_back, name="call_back"),
     ]
