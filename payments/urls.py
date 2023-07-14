7f7eeb02fbe0257a66859b093ccb1552e5dac79f
chris ribia <chrisribia@gmail.com>
2023-07-14 11:29:31 +0300

    mpesa

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import  home
+from .views import  home,getAccessToken,lipa_na_mpesa_online
 
 
 urlpatterns = [
-    path('', home, name='home'),      
+    path('', home, name='home'),  
+    path('access/token', getAccessToken, name='get_mpesa_access_token'),
+    path('online/lipa', lipa_na_mpesa_online, name='lipa_na_mpesa'),    
     ]
