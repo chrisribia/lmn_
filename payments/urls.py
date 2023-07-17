6462678733a162a78f26d7fa97898d475a5c1e1e
chrisribia <31062468+chrisribia@users.noreply.github.com>
2023-07-17 10:47:13 +0100

    Update urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -5,10 +5,10 @@ from .views import  home,getAccessToken,lipa_na_mpesa_online,register_urls,confi
 urlpatterns = [
     path('', home, name='home'),  
     path('access/token', getAccessToken, name='get_mpesa_access_token'),
-    path('online/lipa', lipa_na_mpesa_online, name='lmn'),    
+    path('lmn', lipa_na_mpesa_online, name='lmn'),    
 
     # register, confirmation, validation and callback urls
-    path('c2b/register', register_urls, name="register_mpesa_validation"),
+    path('register', register_urls, name="register"),
     path('c2b/confirmation', confirmation, name="confirmation"),
     path('c2b/validation', validation, name="validation"),
     path('c2b/callback', call_back, name="call_back"),
